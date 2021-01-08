547e6efaac45602397031815db303eb55ba618cd
Mateusz Bogolubow <mateusz@bogolubow.com>
2021-01-08 10:24:42 +0100

    Remove edit warning

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
             <small>v{process.env.REACT_APP_VERSION}</small></p>
         <a
           className="App-link"
